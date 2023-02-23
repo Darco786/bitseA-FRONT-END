b8af1a1d615636bebabe3ce6d20894563fba14d6
draco <122390064+Darco786@users.noreply.github.com>
2023-02-24 02:02:58 +0530

    Update RoadMaps.js

--- src/components/RoadMaps.js
+++ src/components/RoadMaps.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from "react";
 
 const RoadMaps = () => {
   return (
-    <div>RoadMaps</div>
-  )
-}
+    <section id="roadmap" className="mt-12 mb-6">
+      <div className="text-center mb-20">
+        <h1 className="text-white mt-32 mb-20 font-primary font-[700] text-4xl leading-[46px] text-center">
+          ROADMAP
+        </h1>
+      </div>
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="box-1">
+            <div className="bg-gradient-to-r from-[#3C2B9E] to-[#8D08EB] rounded-[85px] text-center">
+              <h1>PHASE 2</h1>
+            </div>
 
-export default RoadMaps
+            <div>
+              <ul>
+                <li> STAKING</li>
+                <li>FULL DAPP RELEASE</li>
+                <li> LAUNCH PAD PLEASE</li>
+                <li> BUILS COMUNITY</li>
+                <li> PARTNERSHIP</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="box-2">
+            <div className="bg-gradient-to-r from-[#3C2B9E] to-[#8D08EB] rounded-[85px] text-center">
+              <h1>PHASE 2</h1>
+            </div>
+
+            <div>
+              <ul>
+                <li> STAKING</li>
+                <li>FULL DAPP RELEASE</li>
+                <li> LAUNCH PAD PLEASE</li>
+                <li> BUILS COMUNITY</li>
+                <li> PARTNERSHIP</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="box-1">
+            <div className="bg-gradient-to-r from-[#3C2B9E] to-[#8D08EB] rounded-[85px] text-center">
+              <h1>PHASE 2</h1>
+            </div>
+
+            <div>
+              <ul>
+                <li> STAKING</li>
+                <li>FULL DAPP RELEASE</li>
+                <li> LAUNCH PAD PLEASE</li>
+                <li> BUILS COMUNITY</li>
+                <li> PARTNERSHIP</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="box-2">
+            <div className="bg-gradient-to-r from-[#3C2B9E] to-[#8D08EB] rounded-[85px] text-center">
+              <h1>PHASE 2</h1>
+            </div>
+
+            <div>
+              <ul>
+                <li> STAKING</li>
+                <li>FULL DAPP RELEASE</li>
+                <li> LAUNCH PAD PLEASE</li>
+                <li> BUILS COMUNITY</li>
+                <li> PARTNERSHIP</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RoadMaps;
