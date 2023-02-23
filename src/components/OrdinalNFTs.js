a635b5dc48f9b51081f535e6fa9979ded0f93b7b
unknown <anas.452111@gmail.com>
2023-02-23 21:57:58 +0500

    nft cards updated

--- src/components/OrdinalNFTs.js
+++ src/components/OrdinalNFTs.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
+import img1 from "../assets/nft (1).png";
+import img2 from "../assets/nft (2).png";
+import img3 from "../assets/nft (3).png";
+import img4 from "../assets/nft (4).png";
+import img5 from "../assets/nft (5).png";
 
 const OrdinalNFTs = () => {
   return (
-    <div>
-        <h1 className="text-white mt-32 mb-14 font-primary font-[700] text-4xl leading-[46px] text-center">
+    <div className="mb-20">
+      <h1 className="text-white mt-32 mb-20 font-primary font-[700] text-4xl leading-[46px] text-center">
         ORDINAL NFTs
       </h1>
+      <div className="flex justify-between items-center">
+        <div className="h-60 w-60  rounded-lg flex flex-col items-center justify-center">
+          <img className="h-full w-full" src={img2} />
+        </div>
+        <div className="h-64 w-60 rounded-lg flex flex-col items-center justify-center">
+          {" "}
+          <img className="h-full w-full" src={img1} />
+        </div>
+        <div className="h-72 w-60 rounded-lg flex flex-col items-center justify-center">
+          {" "}
+          <img className="h-full w-full" src={img5} />
+        </div>
+        <div className="h-64 w-60 rounded-lg flex flex-col items-center justify-center">
+          {" "}
+          <img className="h-full w-full" src={img4} />
+        </div>
+        <div className="h-60 w-60 rounded-lg flex flex-col items-center justify-center">
+          {" "}
+          <img className="h-full w-full" src={img3} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrdinalNFTs
+export default OrdinalNFTs;
