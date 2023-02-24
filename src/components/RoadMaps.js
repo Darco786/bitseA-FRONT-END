854e0cace624319f8a1b66fd21c24664e2a49882
unknown <anas.452111@gmail.com>
2023-02-25 00:12:12 +0500

    Road map responsive fixed

--- src/components/RoadMaps.js
+++ src/components/RoadMaps.js
@@ -1,21 +1,23 @@
 import React from "react";
-import Line from '../assets/Line.png'
+import Line from "../assets/Line.png";
+import designImg from "../assets/design.png";
 const RoadMaps = () => {
   return (
-    <section id="roadmap" className="mt-12 mb-6 pb-60">
-      <div className="text-center mb-20">
+    <section id="roadmap" className="mt-12 mb-6 pb-60 mx-auto">
+      <div className="text-center mb-20 ">
         <h1 className="text-white mt-32 mb-20 font-primary font-[700] text-4xl leading-[46px] text-center">
           ROADMAP
         </h1>
       </div>
-      <div className="container">
-        <div className="  flex justify-around mb-32 flex-wrap ">
+      <div className="container mx-auto">
+        <div className=" mx-auto flex justify-around mb-32 flex-wrap ">
           <div className="box-1">
             <div className="bg-gradient-to-r from-[#3C2B9E] to-[#8D08EB] rounded-[85px] text-center phase-btn">
               <h1>PHASE 2</h1>
             </div>
 
-            <div className="sub-box">
+            <div className="sub-box w-[80vw] z-50 sm:w-[400px] md:w-[450px] lg:w-[500px] relative">
+              <img src={designImg} className="absolute inset-0" />
               <ul>
                 <li> STAKING</li>
                 <li>FULL DAPP RELEASE</li>
@@ -31,7 +33,8 @@ const RoadMaps = () => {
               <h1>PHASE 1</h1>
             </div>
 
-            <div className="sub-box">
+            <div className="sub-box w-[80vw] z-50 sm:w-[400px] md:w-[450px] lg:w-[500px] relative">
+              <img src={designImg} className="absolute inset-0" />
               <ul>
                 <li> CONCEPT BUILDING</li>
                 <li> STEALTH LAUNCH</li>
@@ -43,19 +46,20 @@ const RoadMaps = () => {
           </div>
         </div>
       </div>
-<div>
-  <img src={Line} alt="" className="line-road"/>
-</div>
-      <div className="container">
+      <div>
+        <img src={Line} alt="" className="line-road z-10" />
+      </div>
+      <div className="container mx-auto">
         <div className="flex justify-around mt-16 flex-wrap">
           <div className="box-1">
             <div className="bg-gradient-to-r from-[#3C2B9E] to-[#8D08EB] rounded-[85px] text-center phase-btn">
               <h1>PHASE 4</h1>
             </div>
 
-            <div className="sub-box">
+            <div className="sub-box w-[80vw] z-50 sm:w-[400px] md:w-[450px] lg:w-[500px] relative">
+              <img src={designImg} className="absolute inset-0" />
               <ul>
-                <li>  MAJOR MARKETING CAMPAIGN AFTER FULL </li>
+                <li> MAJOR MARKETING CAMPAIGN AFTER FULL </li>
                 <li>PRODUCT LAUNCH</li>
                 <li> TIER 1 CEX LISTING</li>
                 <li> MAJOR NFT PARTNERSHIPS</li>
@@ -70,14 +74,11 @@ const RoadMaps = () => {
               <h1>PHASE 3</h1>
             </div>
 
-            <div className="sub-box2">
+            <div className="sub-box w-[80vw] z-50 sm:w-[400px] md:w-[450px] lg:w-[500px] h-fit relative">
+              <img src={designImg} className="absolute inset-0" />
               <ul>
-
-
-
                 <li> EXCHANGE LISTING</li>
                 <li>V.2.0 LAUNCH</li>
-                
               </ul>
             </div>
           </div>
