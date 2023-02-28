7e45b0f8169ce37b9f5ca8510f69a05b71b0d15f
draco <122390064+Darco786@users.noreply.github.com>
2023-03-01 00:59:51 +0530

    Update Tokenomics.js

--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -15,40 +15,46 @@ export default function Tokenomics() {
                             <h1 className="text-white font-primary font-[700] text-2xl leading-[31px]">TOKENOMICS</h1>
                             <p className="text-white font-primary font-[500] text-base leading-[20px] mt-[18px]">TOTAL SUPPLY 21,000,000</p>
                             <div className="mt-[43px] flex flex-col space-y-2.5">
-                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]">
+                                <div className="flex justify-between gap-4">
+                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px] w-[100%]">
                                     <div className="w-[21px] h-[21px] bg-gray rounded-[8px] flex"></div>
-                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1">Marketing</div>
+                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1 mr-16">Marketing</div>
                                     <div className="text-white font-primary font-[600] text-base leading-[20px]">10%</div>
                                 </div>
-                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]">
+                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]  w-[100%]">
                                     <div className="w-[21px] h-[21px] bg-gray rounded-[8px] flex"></div>
-                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1">Team/Early Backers</div>
+                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1 mr-16">Team/Early Backers</div>
                                     <div className="text-white font-primary font-[600] text-base leading-[20px]">10%</div>
                                 </div>
-                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]">
+                                </div>
+                                <div className="flex justify-between gap-4">
+                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]  w-[100%]">
                                     <div className="w-[21px] h-[21px] bg-gray rounded-[8px] flex"></div>
-                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1">Liquidity</div>
+                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1 mr-16">Liquidity</div>
                                     <div className="text-white font-primary font-[600] text-base leading-[20px]">40%</div>
                                 </div>
-                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]">
+                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px] w-[100%]">
                                     <div className="w-[21px] h-[21px] bg-gray rounded-[8px] flex"></div>
-                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1">Airdrop</div>
+                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1 mr-16">Airdrop</div>
                                     <div className="text-white font-primary font-[600] text-base leading-[20px]">20%</div>
                                 </div>
-                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]">
+                                </div>
+                                <div className="flex justify-between gap-4">
+                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px] w-[100%]">
                                     <div className="w-[21px] h-[21px] bg-gray rounded-[8px] flex"></div>
-                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1">Partnerships</div>
+                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1 mr-16">Partnerships</div>
                                     <div className="text-white font-primary font-[600] text-base leading-[20px]">5%</div>
                                 </div>
-                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]">
+                                <div className="rounded-[17px] bg-gray-primary px-[16px] pt-[14px] pb-[13px] flex justify-between space-x-[19px]  w-[100%]">
                                     <div className="w-[21px] h-[21px] bg-gray rounded-[8px] flex"></div>
-                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1">Treasury</div>
+                                    <div className="text-white font-primary font-[600] text-base leading-[20px] flex-1 mr-16">Treasury</div>
                                     <div className="text-white font-primary font-[600] text-base leading-[20px]">10%</div>
+                                </div>
                                 </div>
 
                             </div>
                         </div>
-                        <div className="flex relative items-center mt-24">
+                        <div className="items-center ">
                             <img className="" src={radarChart} alt='' />
                             <p className="text-white font-primary font-[600] text-base leading-[20px] text-center mt-[19px]">TAX: 0%</p>
                         </div>
